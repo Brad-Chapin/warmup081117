601d4bf5db8a3ff04590fe517e400e6174ec3ccd
Brad Chapin <paledoor@yahoo.com>
2017-08-11 14:54:19 -0700

    initial commit, adding base files

---
+++ arraytolist.js
@@ -0,0 +1,27 @@
+function arrayToList (arr) {
+  let list = {};
+  for (let i = (arr.length -1); i > 0; i--) {
+    
+  };
+};
+
+function listToArray (list) {
+
+};
+
+function prepend (ele, list) {
+
+};
+
+function nth (list, num) {
+
+};
+
+// console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+// console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+// console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+// console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
