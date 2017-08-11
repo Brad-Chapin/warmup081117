ce854b7ac960f7f39c13c4ea39b63c084c0c974d
Brad Chapin <paledoor@yahoo.com>
2017-08-11 15:19:33 -0700

    first two functions written and working

--- arraytolist.js
+++ arraytolist.js
@@ -1,12 +1,20 @@
 function arrayToList (arr) {
-  let list = {};
-  for (let i = (arr.length -1); i > 0; i--) {
-    
+  let list = null;
+  for (let i = (arr.length - 1); i >= 0; i--) {
+    list = {
+      value: arr[i],
+      rest: list
+    }
   };
+  return list;
 };
 
 function listToArray (list) {
-
+  let arr = [];
+  for (let next = list; next; next = next.rest) {
+    arr.push(next.value);
+  }
+  return arr;
 };
 
 function prepend (ele, list) {
@@ -19,7 +27,7 @@ function nth (list, num) {
 
 // console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 // console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
