dd97d90850a8587a58e2568fd01136223b519797
Brad Chapin <paledoor@yahoo.com>
2017-08-15 11:07:39 -0700

    finalized array to list, list to array, prepend, and nth functions

--- arraytolist.js
+++ arraytolist.js
@@ -18,18 +18,22 @@ function listToArray (list) {
 };
 
 function prepend (ele, list) {
-
+  let newList = {
+    value: ele,
+    rest: list
+  }
+  return newList;
 };
 
 function nth (list, num) {
-
+  return (listToArray(list)[num]);
 };
 
-// console.log(arrayToList([10, 20]));
+console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-// console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
